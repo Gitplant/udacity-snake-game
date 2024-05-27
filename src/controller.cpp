cd1d39f88d67083fbc97a9f995f360d6fdec651b
Gitplant <79708230+Gitplant@users.noreply.github.com>
2024-05-27 10:16:04 +0200

    Clean up controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -2,87 +2,51 @@
 #include <iostream>
 #include "SDL.h"
 #include "snake.h"
-#include "game.h" // pause-game
-#include "player.h"  // player-class
+#include "game.h"
+#include "player.h"
 
-// comment for pause-game
-// void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
-//                                  Snake::Direction opposite) const {
-//   if (snake.direction != opposite || snake.size == 1) snake.direction = input;
-//   return;
-// }
-
-// void Controller::HandleInput(bool &running, Snake &snake) const {
-// void Controller::HandleInput(bool &running, Game* game) const {
-  void Controller::HandleInput(bool &running, std::vector<Player> &players, Game* game) const {
+// Handle the keyboard input from the user
+void Controller::HandleInput(bool &running, std::vector<Player> &players, Game* game) const {
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
       running = false;
-    } else if (e.type == SDL_KEYDOWN) {
-        switch (e.key.keysym.sym) {
-          case SDLK_SPACE:
-            game->PauseGame();  // pause-game
-            break;
-          // PLAYER 1 uses arrows
-          case SDLK_UP:
-            // ChangeDirection(snake, Snake::Direction::kUp,
-            //                 Snake::Direction::kDown);
-            // game->ChangeSnakeDirection(Snake::Direction::kUp, Snake::Direction::kDown);  // pause-game
-            // game->ChangeSnakeDirection(1, Snake::Direction::kUp, Snake::Direction::kDown);  // pause-game, two-player
-            players[0].snake->ChangeDirection(Snake::Direction::kUp, Snake::Direction::kDown);  // pause-game, two-player, player-class
-            break;
+      }
+    else if (e.type == SDL_KEYDOWN) {
+      switch (e.key.keysym.sym) {
+        // Pause the game
+        case SDLK_SPACE:
+          game->PauseGame();
+          break;
 
-          case SDLK_DOWN:
-            // ChangeDirection(snake, Snake::Direction::kDown,
-            //                 Snake::Direction::kUp);
-            // game->ChangeSnakeDirection(Snake::Direction::kDown, Snake::Direction::kUp);  // pause-game
-            // game->ChangeSnakeDirection(1, Snake::Direction::kDown, Snake::Direction::kUp);  // pause-game, two-player
-            players[0].snake->ChangeDirection(Snake::Direction::kDown, Snake::Direction::kUp);  // pause-game, two-player
-            break;
+        // PLAYER 1 uses arrows
+        case SDLK_UP:
+          players[0].snake->ChangeDirection(Snake::Direction::kUp, Snake::Direction::kDown);
+          break;
+        case SDLK_DOWN:
+          players[0].snake->ChangeDirection(Snake::Direction::kDown, Snake::Direction::kUp);
+          break;
+        case SDLK_LEFT:
+          players[0].snake->ChangeDirection(Snake::Direction::kLeft, Snake::Direction::kRight);
+          break;
+        case SDLK_RIGHT:
+          players[0].snake->ChangeDirection(Snake::Direction::kRight, Snake::Direction::kLeft);
+          break;
 
-          case SDLK_LEFT:
-            // ChangeDirection(snake, Snake::Direction::kLeft,
-            //                 Snake::Direction::kRight);
-            // game->ChangeSnakeDirection(Snake::Direction::kLeft, Snake::Direction::kRight);  // pause-game
-            players[0].snake->ChangeDirection(Snake::Direction::kLeft, Snake::Direction::kRight);  // pause-game, two-player
-            break;
-
-          case SDLK_RIGHT:
-            // ChangeDirection(snake, Snake::Direction::kRight,
-            //                 Snake::Direction::kLeft);
-            // game->ChangeSnakeDirection(Snake::Direction::kRight, Snake::Direction::kLeft);  // pause-game
-            players[0].snake->ChangeDirection(Snake::Direction::kRight, Snake::Direction::kLeft);  // pause-game, two-player
-            break;
-        // case SDLK_SPACE:
-        //   game->PauseGame();
-
-        // PLAYER 2 uses WASD
+        // PLAYER 2 uses WASD keys
         case SDLK_w:
-      //     // game->ChangeSnakeDirection(2, Snake::Direction::kUp, Snake::Direction::kDown);  // two-player
           players[1].snake->ChangeDirection(Snake::Direction::kUp, Snake::Direction::kDown);
           break;
-
         case SDLK_s:
-      //     // game->ChangeSnakeDirection(2, Snake::Direction::kDown, Snake::Direction::kUp);  // two-player
-          players[1].snake->ChangeDirection(Snake::Direction::kDown, Snake::Direction::kUp);  // two-player
+          players[1].snake->ChangeDirection(Snake::Direction::kDown, Snake::Direction::kUp);
           break;
-
         case SDLK_a:
-          // game->ChangeSnakeDirection(2, Snake::Direction::kLeft, Snake::Direction::kRight);  // two-player
-          players[1].snake->ChangeDirection(Snake::Direction::kLeft, Snake::Direction::kRight);  // two-player
+          players[1].snake->ChangeDirection(Snake::Direction::kLeft, Snake::Direction::kRight);
           break;
-
         case SDLK_d:
-      //     // game->ChangeSnakeDirection(2, Snake::Direction::kRight, Snake::Direction::kLeft);  // two-player
-          players[1].snake->ChangeDirection(Snake::Direction::kRight, Snake::Direction::kLeft);  // two-player
+          players[1].snake->ChangeDirection(Snake::Direction::kRight, Snake::Direction::kLeft);
           break;
       }
     }
   }
 }
-
-// temp
-// Controller::Controller(const Controller& other) {
-//     std::cout << "  -------------------------   Controller copy constructor called." << std::endl;
-//     }
--- src/controller.h
+++ src/controller.h
@@ -3,22 +3,14 @@
 
 #include "snake.h"
 
-class Game; // pause-game: Forward declaration
-class Player;  // player-class: Forward declaration
+class Game; // Forward declaration
+class Player;  // Forward declaration
 
 class Controller {
  public:
-//   void HandleInput(bool &running, Snake &snake) const;
-// void HandleInput(bool &running, Snake &snake, Game* game) const;
   void HandleInput(bool &running, std::vector<Player> &players, Game* game) const;
 
-  // Controller(){};  // temp
-  // Controller(const Controller& other);  // temp
-
-
  private:
-//   void ChangeDirection(Snake &snake, Snake::Direction input,
-//                        Snake::Direction opposite) const;
 };
 
 #endif
