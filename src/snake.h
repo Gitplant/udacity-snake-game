82bbacfde8f82e9fc6f952b92bbfbe95fca5c2d0
Gitplant <79708230+Gitplant@users.noreply.github.com>
2024-05-28 12:30:04 +0200

    Do not use SDL_FPoint

--- src/snake.h
+++ src/snake.h
@@ -11,6 +11,11 @@ struct Color{
     Uint8 a;
   };
 
+struct FPoint{
+  float x;
+  float y;
+};
+
 class Snake {
  public:
   Snake(int grid_width, int grid_height, float head_x, Color color = Color{255, 255, 255, 255});
@@ -21,7 +26,7 @@ class Snake {
   Color GetColor() const {return _color;};
   int GetSize() const {return _size;};
   bool IsAlive() const {return _alive;};
-  SDL_FPoint GetHead() const {return _head;};
+  FPoint GetHead() const {return _head;};
   SDL_Point GetHeadInt() const;
   std::vector<SDL_Point> GetBody() const {return _body;};
   bool HasMoved() const {return _has_moved;};
@@ -43,7 +48,7 @@ class Snake {
   int const _grid_height;
   float _speed{0.1f};
   int _size{1};
-  SDL_FPoint _head;
+  FPoint _head;
   std::vector<SDL_Point> _body;
 
   Direction _direction = Direction::kUp;
